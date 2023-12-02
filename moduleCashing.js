e17cb0cc45803c2125baaa30a13595cc820a02b1
Devapujyula <sdevapujyula@gmail.com>
2023-12-02 19:08:44 +0530

    added module cashing code

---
+++ moduleCashing.js
@@ -0,0 +1,17 @@
+// cashing is simple word for remembering
+
+class SuperHero {
+    constructor(name) {
+        this.name = name
+    }
+
+    getName() {
+        return this.name
+    }
+
+    setName(name) {
+        this.name = name
+    }
+}
+
+module.exports = new SuperHero("Batman")
