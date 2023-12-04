0abd38ed219b5def3f3f4ee541dfed125a8cff77
Devapujyula <sdevapujyula@gmail.com>
2023-12-04 14:56:01 +0530

    added support module

--- add.js
+++ add.js
@@ -1,10 +1,8 @@
 const add = (a, b) => {
-    return a + b
-}
-
+  return a + b;
+};
 
 // with the this module.export we are using only the add functionality of which if we want only one functiona;ity from the entire module
 module.exports = add;
 
 // console.log(add(9,6))
-
