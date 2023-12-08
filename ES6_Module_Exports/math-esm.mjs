adfc618f4451f912e491c679ae2c703f2c6f2c73
Devapujyula <sdevapujyula@gmail.com>
2023-12-08 14:37:25 +0530

    added import and export module

---
+++ ES6_Module_Exports/math-esm.mjs
@@ -0,0 +1,36 @@
+// const add = (a,b) => {
+//     return a + b
+// }
+
+// export default add
+
+// 2nd way of exporting ES-6 module
+//  export default (a,b) => {
+//     return a + b
+//  }
+
+// 3rd way of exporting module
+
+// const add = (a, b) => {
+//     return a + b
+// }
+
+// const subtract = (a, b) => {
+//     return a - b
+// }
+
+
+// 4th way of exporting
+
+export const add = (a, b) => {
+    return a + b
+}
+
+export const subtract = (a, b) => {
+    return a - b
+}
+
+// export default {
+//     add,
+//     subtract
+// }
