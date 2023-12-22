3e84589df6bd5ffaeb078b1cb11a8918e08c2b6f
Devapujyula <sdevapujyula@gmail.com>
2023-12-22 22:23:06 +0530

    added http module

---
+++ 13https.js
@@ -0,0 +1,10 @@
+const http = require("node:http");
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hello Prakash");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
